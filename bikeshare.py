84c8c06aa5b6c2860b047f68e55e983eebbc1a70
Martin Berriel <martinberriel@gmail.com>
2021-02-15 19:23:58 -0300

    refactor: Rename variable on time calculation function
    Rename secs variable on dateStringFromSeconds to seconds

--- bikeshare.py
+++ bikeshare.py
@@ -145,13 +145,13 @@ def station_stats(df):
     print('-'*40)
 
 
-def dateStringFromSeconds(secs):
+def dateStringFromSeconds(seconds):
     """Return days, hours , minutes and seconds as a string for a given number of seconds."""
-    if secs > 0:
-        days = secs//86400
-        hours = (secs - days*86400)//3600
-        minutes = (secs - days*86400 - hours*3600)//60
-        seconds = secs - days*86400 - hours*3600 - minutes*60
+    if seconds > 0:
+        days = seconds//86400
+        hours = (seconds - days*86400)//3600
+        minutes = (seconds - days*86400 - hours*3600)//60
+        seconds = seconds - days*86400 - hours*3600 - minutes*60
         result = ("{0} day{1}, ".format(int(days), "s" if days!=1 else "") if days else "") + \
         ("{0} hour{1}, ".format(int(hours), "s" if hours!=1 else "") if hours else "") + \
         ("{0} minute{1}, ".format(int(minutes), "s" if minutes!=1 else "") if minutes else "") + \
