f86046f201f8de5c523621d8395222574c24243c
Martin Berriel <martinberriel@gmail.com>
2021-02-15 19:19:00 -0300

    refactor: Change retry prompt
    Change retry text to make it more user friendly

--- bikeshare.py
+++ bikeshare.py
@@ -251,7 +251,7 @@ def main():
         
         display_raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to start over? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
